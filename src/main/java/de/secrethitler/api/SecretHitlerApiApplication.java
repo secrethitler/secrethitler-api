ccd437c629c6a72966f8bd88286767db4aebe034
Collin Alpert <collinalpert@gmail.com>
2019-08-19 14:26:52 +0200

    Implemented route for creating games

--- src/main/java/de/secrethitler/api/SecretHitlerApiApplication.java
+++ src/main/java/de/secrethitler/api/SecretHitlerApiApplication.java
@@ -3,9 +3,18 @@ package de.secrethitler.api;
 import com.github.collinalpert.java2db.database.DBConnection;
 import de.secrethitler.api.config.DatabaseConfiguration;
 import de.secrethitler.api.config.PusherConfiguration;
+import de.secrethitler.api.modules.ChannelNameModule;
+import de.secrethitler.api.services.GameService;
+import de.secrethitler.api.services.LinkedUserGameRoleService;
+import de.secrethitler.api.services.PolicyService;
+import de.secrethitler.api.services.RoleService;
+import de.secrethitler.api.services.RoundService;
+import de.secrethitler.api.services.UserService;
+import de.secrethitler.api.services.VoteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -22,5 +31,45 @@ public class SecretHitlerApiApplication {
 		DBConnection.DATABASE = databaseConfig.getDatabaseName();
 		DBConnection.USERNAME = databaseConfig.getUsername();
 		DBConnection.PASSWORD = databaseConfig.getPassword();
+	}
+
+	@Bean
+	GameService getGameService() {
+		return new GameService();
+	}
+
+	@Bean
+	LinkedUserGameRoleService getLinkedUserGameRoleService() {
+		return new LinkedUserGameRoleService();
+	}
+
+	@Bean
+	PolicyService getPolicyService() {
+		return new PolicyService();
+	}
+
+	@Bean
+	RoleService getRoleService() {
+		return new RoleService();
+	}
+
+	@Bean
+	RoundService getRoundService() {
+		return new RoundService();
+	}
+
+	@Bean
+	UserService getUserService() {
+		return new UserService();
+	}
+
+	@Bean
+	VoteService getVoteService() {
+		return new VoteService();
+	}
+
+	@Bean
+	ChannelNameModule getChannelNameModule() {
+		return new ChannelNameModule();
 	}
 }
