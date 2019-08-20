3a9a72cdf7f1f74e370f4ce692aaa772679bec1b
Collin Alpert <collinalpert@gmail.com>
2019-08-20 14:33:02 +0200

    Pusher authentication (#5)
    * Added Pusher authentication
    
    * Added pusher authentication
    
    * Fixed reading from pusher config
    
    * Added creator check

---
+++ src/main/java/de/secrethitler/api/controllers/PusherController.java
@@ -0,0 +1,64 @@
+package de.secrethitler.api.controllers;
+
+import com.pusher.rest.data.PresenceUser;
+import de.secrethitler.api.modules.PusherModule;
+import de.secrethitler.api.services.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * @author Collin Alpert
+ */
+@RestController
+@RequestMapping("/api/pusher")
+public class PusherController {
+
+	private final String socketIdParameter = "socket_id";
+	private final String channelNameParameter = "channel_name";
+	private final String userIdParameter = "user_id";
+	private final String userNameParameter = "user_name";
+
+	private final PusherModule pusherModule;
+	private final GameService gameService;
+
+	public PusherController(PusherModule pusherModule, GameService gameService) {
+		this.pusherModule = pusherModule;
+		this.gameService = gameService;
+	}
+
+	@CrossOrigin(origins = "*")
+	@PostMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<String> authenticatePresence(@RequestParam Map<String, Object> requestBody, HttpSession session) {
+		var pusher = this.pusherModule.getPusherInstance();
+		var socketId = (String) requestBody.get(this.socketIdParameter);
+		var channelName = (String) requestBody.get(this.channelNameParameter);
+
+		if (channelName.startsWith("presence")) {
+			var userId = (long) session.getAttribute(this.userIdParameter);
+			var userName = (String) session.getAttribute(this.userNameParameter);
+
+			boolean isChannelCreator = this.gameService.getCreatorIdByChannelName(channelName) == userId;
+			var responseData = Map.of(this.userNameParameter, userName, "is_channel_creator", isChannelCreator);
+
+			return ResponseEntity.ok(pusher.authenticate(socketId, channelName, new PresenceUser(userId, responseData)));
+		} else if (channelName.startsWith("private")) {
+			var userId = Long.parseLong(((String) requestBody.get(this.userIdParameter)).split("-")[1]);
+			if (userId != ((long) session.getAttribute(this.userIdParameter))) {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+			}
+
+			return ResponseEntity.ok(pusher.authenticate(socketId, channelName));
+		}
+
+		return ResponseEntity.badRequest().build();
+	}
+}
--- src/main/java/de/secrethitler/api/services/GameService.java
+++ src/main/java/de/secrethitler/api/services/GameService.java
@@ -7,4 +7,8 @@ import de.secrethitler.api.entities.Game;
  * @author Collin Alpert
  */
 public class GameService extends BaseService<Game> {
+
+	public long getCreatorIdByChannelName(String channelName) {
+		return getSingle(x -> x.getChannelname() == channelName).project(Game::getCreatorid).first().orElse(0L);
+	}
 }
